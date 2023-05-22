55614fc0f166d5dad9db054dd87df3b399e41c27
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-22 16:59:03 +0500

    refactor: перевести Messenger на grid

--- src/components/Messenger/Messenger.tsx
+++ src/components/Messenger/Messenger.tsx
@@ -1,13 +1,17 @@
 import Styles from './Messenger.module.css';
 import CreateForm from '../CreateForm/CreateForm';
+import Dialog from '../Dialog/Dialog';
 
 export default function Messenger() {
   return (
     <div className={Styles.window}>
-      <div className={Styles.dialogsList}>
-        <CreateForm />
+      <CreateForm />
+      <div></div>
+      <ul className={Styles.dialogsList}>
+          <Dialog isChecked={false} />
+      </ul>
+      <div className={Styles.chat}>
       </div>
-      <div className={Styles.dialog}></div>
     </div>
   );
 }
