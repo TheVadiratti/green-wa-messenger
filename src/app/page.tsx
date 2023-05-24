d22c7ff75bb33048d331e2e552773f76fa0d5bce
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-24 11:36:43 +0500

    feat: защитить роут мессенжера

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Messenger from '../components/Messenger/Messenger';
 import Styles from './page.module.css';
 
 export default function Home() {
+  const router = useRouter();
+  const apiTokenInstance = sessionStorage.getItem('apiTokenInstance');
+
+  useEffect(() => {
+    if (!apiTokenInstance) {
+      router.push('/login');
+    }
+  }, []);
+
   return (
     <main className={Styles.main}>
       <Messenger />
